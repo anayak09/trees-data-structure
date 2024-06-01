5b93f9ee212d297a09822299f85f2e037f51dec1
Akhilesh Nayak <akhil9494@gmail.com>
2024-06-01 15:09:50 +0530

    First commit

---
+++ src/BinaryTree.java
@@ -0,0 +1,17 @@
+public class BinaryTree {
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public BinaryTree() {
+
+    }
+
+    private Node root;
+}
